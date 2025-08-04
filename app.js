b6fb51817155d421d9de8267a11318bbc417a6d9
Tensen <thienmpham@gmail.com>
2025-08-04 18:54:22 -0400

    Start the base functions

---
+++ app.js
@@ -0,0 +1,27 @@
+function add([nums]) {
+  let total = 0;
+  for (let num of nums) {
+    return (total += num);
+  }
+}
+
+function subtract([nums]) {
+  let total = 0;
+  for (let num of nums) {
+    return (total -= num);
+  }
+}
+
+function multiply([nums]) {
+  let total = 1;
+  for (let num of nums) {
+    return (total *= num);
+  }
+}
+
+function divide([nums]) {
+  let total = 1;
+  for (let num of nums) {
+    return (total /= num);
+  }
+}
