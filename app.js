fe9829ea899c5d9febcab0c8f39c6e916bd17595
Tensen <thienmpham@gmail.com>
2025-08-28 19:38:34 -0400

    Logic is becoming a little hard to track...

--- app.js
+++ app.js
@@ -1,55 +1,135 @@
-function add([nums]) {
+function add(nums) {
   let total = 0;
   for (let num of nums) {
-    return (total += num);
+    total += num;
   }
+  return total;
 }
 
-function subtract([nums]) {
+function subtract(nums) {
   let total = 0;
   for (let num of nums) {
-    return (total -= num);
+    total -= num;
   }
+  return total;
 }
 
-function multiply([nums]) {
+function multiply(nums) {
   let total = 1;
   for (let num of nums) {
-    return (total *= num);
+    total *= num;
   }
+  return total;
 }
 
-function divide([nums]) {
+function divide(nums) {
   let total = 1;
   for (let num of nums) {
-    return (total /= num);
+    total /= num;
+  }
+  return total;
+}
+
+function operate(inputs, operator) {
+  if (operator == "add") {
+    console.log(add(inputs));
+    return add(inputs);
+  }
+  if (operator == "subtract") {
+    console.log(subtract(inputs));
+    return subtract(inputs);
+  }
+  if (operator == "multiply") {
+    console.log(multiply(inputs));
+    return multiply(inputs);
+  }
+  if (operator == "divide") {
+    console.log(divide(inputs));
+    return divide(inputs);
+  }
+}
+function chooseOperator(e) {
+  if (e.target.id == "btn-add") {
+    return "add";
+  }
+  if (e.target.id == "btn-subtract") {
+    return "subtract";
+  }
+  if (e.target.id == "btn-multiply") {
+    return "multiply";
+  }
+  if (e.target.id == "btn-divide") {
+    return "divide";
   }
 }
 
-function operate([inputs], operator) {
-  for (let input of inputs) {
-  }
+function storeInputs(inputs) {
+  console.log("stored:", inputs);
+  let inputsArray = [];
+  inputsArray.push(inputs);
+  console.log("storedArray:", inputsArray);
 }
 
 function displayInputs() {
   let numbers = document.querySelectorAll(".btn");
   let inputs = [];
+  let inputsArray = [];
   let screen = document.querySelector(".screen");
+  let operator;
+  let total;
   // iterate through each array item
+
   for (num of numbers) {
     num.addEventListener("click", function (e) {
-      console.log(e.target.className);
-      if (e.target.className == "btn btn-op") {
-        console.log("op");
+      // initial total
+      if (
+        e.target.className == "btn btn-op" &&
+        screen.innerHTML !== "" &&
+        total == undefined
+      ) {
+        screen.innerHTML = null;
+        console.log("stored:", inputs);
+        inputs = parseInt(inputs);
+        inputsArray.push(inputs);
+        inputs = [];
+        operator = chooseOperator(e);
+        console.log("storedArray:", inputsArray, "op");
+      }
+
+      if (
+        e.target.className == "btn btn-op" &&
+        screen.innerHTML !== "" &&
+        total !== undefined
+      ) {
+        operator = chooseOperator(e);
+        console.log("storedArray:", inputsArray);
+      }
+
+      if (e.target.id == "btn-equals" && screen.innerHTML !== "") {
+        console.log("stored:", inputs);
+        inputs = parseInt(inputs);
+        inputsArray.push(inputs);
+        console.log("storedArray:", inputsArray);
+        console.log("operator = ", operator);
+        // operate(inputsArray, operator);
+        total = operate(inputsArray, operator);
+        screen.innerHTML = total;
+        inputsArray = [];
+        inputsArray.push(total);
       }
       if (e.target.className == "btn btn-num") {
+        screen.innerHTML = null;
+        inputs = [];
+
         handleNumClick(e);
         inputs += assignNumValue(e);
         console.log("inputs:", inputs);
       }
       if (e.target.id == "btn-clear") {
         screen.innerHTML = null;
+        total = null;
         inputs = [];
+        inputsArray = [];
       }
     });
   }
@@ -69,20 +149,8 @@ function assignNumValue(e) {
 
   for (i = 0; i <= 9; i++) {
     if (e.target.id == `btn-${i}`) {
-      input = `${i}`;
+      input = i;
       return input;
     }
   }
 }
-
-// -----IDEAS:
-// Store input into just one variable
-// Once a operation is chosen, then the second
-//set of #'s is a separate array item
-
-// if !btn-num is selected then it will add
-// ... the current value of input
-
-// CURRENT GOAL
-// If an operative btn is pushed
-// ..then start to store the next set of #'s
